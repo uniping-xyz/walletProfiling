4eb2d586f1b5389187669e3ebb046fe08919ce55
saurav <houzier.saurav@gmail.com>
2022-11-03 14:38:55 +0530

    Fixing bug in find_tagged_contracts api

--- src/find_addresses/contract_tags.py
+++ src/find_addresses/contract_tags.py
@@ -70,6 +70,9 @@ async def tags_cache_validity(app: object, cache_validity_hours: int) -> object:
     data = await get_cache(app.config.REDIS_CLIENT,f"{chain}-tags")
     return json.loads(data)
 
+
+
+
 @TOKEN_TAGS_BP.get('find_tags')
 async def find_tags(request):
 
@@ -89,6 +92,29 @@ async def find_tags(request):
 
     return Response.success_response(data=result)
 
+
+def make_query_string(request_args: dict, args_list: list) -> str:
+    query_string = ""
+    for (key, value) in request_args.items():
+        if key in args_list:
+            if type(value) == list:
+                value = value[0]
+            query_string += f"&{key}={value}"
+    return query_string[1:] # to remove the first $ sign appened to the string
+
+async def tags_contracts_cache_validity(app: object, caching_key: str, request_args: dict) -> object:
+    CACHE_EXPIRY = app.config.CACHING_TTL['LEVEL_EIGHT']
+    tags_contracts_cache  = await cache_validity(app.config.REDIS_CLIENT, caching_key, CACHE_EXPIRY)
+    if not tags_contracts_cache:
+        data = await get_tagged_ethereum_contracts(app.config.LUABASE_API_KEY, request_args.get("tag"))
+        await set_cache(app.config.REDIS_CLIENT, caching_key, data)
+        return data
+    data = await get_cache(app.config.REDIS_CLIENT, caching_key)
+    return json.loads(data)
+
+
+
+
 @TOKEN_TAGS_BP.get('find_tagged_contracts')
 async def find_tagged_contracts(request):
 
@@ -97,30 +123,13 @@ async def find_tagged_contracts(request):
 
     if  not request.args.get("tag"):
         raise CustomError("Tag is required")
+    query_string: str = make_query_string(request.args, ["chain", "tag"])
 
     caching_key = f"{request.route.path}?{request.query_string}"
     if request.app.config.CACHING:
-
-        cache_valid = await cache_validity(request.app.config.REDIS_CLIENT, caching_key, 
-                            request.app.config.CACHING_TTL['LEVEL_EIGHT'])
-        if not cache_valid:
-            result = await get_tagged_ethereum_contracts(request.app.config.LUABASE_API_KEY, request.args.get("tag"))
-            await set_cache(request.app.config.REDIS_CLIENT, caching_key, result)
-            return result
-        cached_result= await get_cache(request.app.config.REDIS_CLIENT, caching_key)
-        result =  json.loads(cached_result)
+        caching_key = f"{request.route.path}?{query_string}"
+        logger.info(f"Here is the caching key {caching_key}")
+        data = await tags_contracts_cache_validity(request.app, caching_key, request.args)
     else:
-        result = await get_tagged_ethereum_contracts(request.app.config.LUABASE_API_KEY, request.args.get("tag"))
-    return Response.success_response(data=result)
-
-async def tags_cache_validity(app: object, cache_validity_hours: int) -> object:
-    for chain in app.config.SUPPORTED_CHAINS:
-        tags_cache_validity  = await cache_validity(app.config.REDIS_CLIENT, f"{chain}/tags", cache_validity_hours)
-        if not tags_cache_validity:
-            eth_tags = await get_ethereum_tags(app.config.LUABASE_API_KEY)
-            await set_cache(app.config.REDIS_CLIENT, f"{chain}/tags", [e["label"] for e in eth_tags])
-            return [e["label"] for e in eth_tags]
-    data = await get_cache(app.config.REDIS_CLIENT,f"{chain}/tags")
-    return json.loads(data)
-
-
+        data = await  get_tagged_ethereum_contracts(request.app.config.LUABASE_API_KEY, request.args.get("tag"))
+    return Response.success_response(data=data)
