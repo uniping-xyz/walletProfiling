8cd37415caeabb8b49d61e7d213cbc4fa66c5aa9
saurav <houzier.saurav@gmail.com>
2023-02-07 13:47:31 +0530

    more wallet tags api added

--- src/wallet_tags/api.py
+++ src/wallet_tags/api.py
@@ -12,6 +12,13 @@ from .ethereum.eth_millionaires_w_nfts import eth_millionaires_w_nfts
 from .ethereum.token_millionaires_w_nfts import token_millionaires_w_nfts
 from .ethereum.wallet_millionaires_w_nfts import wallet_millionaires_w_nfts
 
+from .ethereum.wallet_millionaires_cex import wallet_millionaires_cex
+from .ethereum.eth_millionaires_cex import eth_millionaires_cex
+from .ethereum.token_millionaires_cex import token_millionaires_cex
+
+from .ethereum.eth_millionaires_dex import eth_millionaires_dex
+from .ethereum.token_millionaires_dex import token_millionaires_dex
+from .ethereum.wallet_milllionaries_dex import wallet_millionaires_dex
 
 WALLET_TAGS_BP = Blueprint("wallettags", url_prefix='/wallettags', version=1)
 
@@ -25,9 +32,151 @@ def make_query_string(request_args: dict, args_list: list) -> str:
             query_string += f"&{key}={value}"
     return query_string[1:]
 
+#---------------------------- Centralised exchanges ------------------------------------------
+
+async def wallet_millionaires_cex_caching(app: object, caching_key: str, caching_ttl:int, request_args: dict) -> any: 
+    cache_valid = await cache_validity(app.config.REDIS_CLIENT, caching_key, caching_ttl)
+    if not cache_valid:
+        data = await wallet_millionaires_cex()
+        if data:
+            await set_cache(app.config.REDIS_CLIENT, caching_key, data)
+        return data
+    result= await get_cache(app.config.REDIS_CLIENT, caching_key)
+    return json.loads(result)
+
+@WALLET_TAGS_BP.get('<chain>/wallet_millionaires_cex')
+async def wallet_millionaires_cex_handler(request, chain):
+    caching_ttl =  request.app.config.CACHING_TTL['LEVEL_SEVEN']
+    if chain not in request.app.config["SUPPORTED_CHAINS"]:
+        raise CustomError("chain is required")
+    request.args["chain"] = chain
+    caching_key = f"{request.route.path.replace('<chain:str>', chain)}"
+    data = await wallet_millionaires_cex_caching(request.app, caching_key, caching_ttl, request.args)
+    return Response.success_response(data=data, caching_ttl=caching_ttl, days=-1)
+
+
+async def token_millionaires_cex_caching(app: object, caching_key: str, caching_ttl:int, request_args: dict) -> any: 
+    cache_valid = await cache_validity(app.config.REDIS_CLIENT, caching_key, caching_ttl)
+    if not cache_valid:
+        data = await token_millionaires_cex()
+        if data:
+            await set_cache(app.config.REDIS_CLIENT, caching_key, data)
+        return data
+    result= await get_cache(app.config.REDIS_CLIENT, caching_key)
+    return json.loads(result)
+
+@WALLET_TAGS_BP.get('<chain>/token_millionaires_cex')
+async def token_millionaires_cex_handler(request, chain):
+    caching_ttl =  request.app.config.CACHING_TTL['LEVEL_SEVEN']
+    if chain not in request.app.config["SUPPORTED_CHAINS"]:
+        raise CustomError("chain is required")
+    request.args["chain"] = chain
+    caching_key = f"{request.route.path.replace('<chain:str>', chain)}"
+    data = await token_millionaires_cex_caching(request.app, caching_key, caching_ttl, request.args)
+    return Response.success_response(data=data, caching_ttl=caching_ttl, days=-1)
+
+
+async def eth_millionaires_cex_caching(app: object, caching_key: str, caching_ttl:int, request_args: dict) -> any: 
+    cache_valid = await cache_validity(app.config.REDIS_CLIENT, caching_key, caching_ttl)
+    if not cache_valid:
+        data = await eth_millionaires_cex()
+        if data:
+            await set_cache(app.config.REDIS_CLIENT, caching_key, data)
+        return data
+    result= await get_cache(app.config.REDIS_CLIENT, caching_key)
+    return json.loads(result)
+
+@WALLET_TAGS_BP.get('<chain>/eth_millionaires_cex')
+async def eth_millionaires_cex_handler(request, chain):
+    caching_ttl =  request.app.config.CACHING_TTL['LEVEL_SEVEN']
+    if chain not in request.app.config["SUPPORTED_CHAINS"]:
+        raise CustomError("chain is required")
+    request.args["chain"] = chain
+    caching_key = f"{request.route.path.replace('<chain:str>', chain)}"
+    data = await eth_millionaires_cex_caching(request.app, caching_key, caching_ttl, request.args)
+    return Response.success_response(data=data, caching_ttl=caching_ttl, days=-1)
+
+
+
+#---------------------------- DECentralised exchanges ------------------------------------------
+
+async def wallet_millionaires_dex_caching(app: object, caching_key: str, caching_ttl:int, request_args: dict) -> any: 
+    cache_valid = await cache_validity(app.config.REDIS_CLIENT, caching_key, caching_ttl)
+    if not cache_valid:
+        data = await wallet_millionaires_dex()
+        if data:
+            await set_cache(app.config.REDIS_CLIENT, caching_key, data)
+        return data
+    result= await get_cache(app.config.REDIS_CLIENT, caching_key)
+    return json.loads(result)
+
+@WALLET_TAGS_BP.get('<chain>/wallet_millionaires_dex')
+async def wallet_millionaires_dex_handler(request, chain):
+    caching_ttl =  request.app.config.CACHING_TTL['LEVEL_SEVEN']
+    if chain not in request.app.config["SUPPORTED_CHAINS"]:
+        raise CustomError("chain is required")
+    request.args["chain"] = chain
+    caching_key = f"{request.route.path.replace('<chain:str>', chain)}"
+    data = await wallet_millionaires_dex_caching(request.app, caching_key, caching_ttl, request.args)
+    return Response.success_response(data=data, caching_ttl=caching_ttl, days=-1)
+
+
+async def token_millionaires_dex_caching(app: object, caching_key: str, caching_ttl:int, request_args: dict) -> any: 
+    cache_valid = await cache_validity(app.config.REDIS_CLIENT, caching_key, caching_ttl)
+    if not cache_valid:
+        data = await token_millionaires_dex()
+        if data:
+            await set_cache(app.config.REDIS_CLIENT, caching_key, data)
+        return data
+    result= await get_cache(app.config.REDIS_CLIENT, caching_key)
+    return json.loads(result)
+
+@WALLET_TAGS_BP.get('<chain>/token_millionaires_dex')
+async def token_millionaires_dex_handler(request, chain):
+    caching_ttl =  request.app.config.CACHING_TTL['LEVEL_SEVEN']
+    if chain not in request.app.config["SUPPORTED_CHAINS"]:
+        raise CustomError("chain is required")
+    request.args["chain"] = chain
+    caching_key = f"{request.route.path.replace('<chain:str>', chain)}"
+    data = await token_millionaires_dex_caching(request.app, caching_key, caching_ttl, request.args)
+    return Response.success_response(data=data, caching_ttl=caching_ttl, days=-1)
+
+
+async def eth_millionaires_dex_caching(app: object, caching_key: str, caching_ttl:int, request_args: dict) -> any: 
+    cache_valid = await cache_validity(app.config.REDIS_CLIENT, caching_key, caching_ttl)
+    if not cache_valid:
+        data = await eth_millionaires_dex()
+        if data:
+            await set_cache(app.config.REDIS_CLIENT, caching_key, data)
+        return data
+    result= await get_cache(app.config.REDIS_CLIENT, caching_key)
+    return json.loads(result)
+
+@WALLET_TAGS_BP.get('<chain>/eth_millionaires_dex')
+async def eth_millionaires_dex_handler(request, chain):
+    caching_ttl =  request.app.config.CACHING_TTL['LEVEL_SEVEN']
+    if chain not in request.app.config["SUPPORTED_CHAINS"]:
+        raise CustomError("chain is required")
+    request.args["chain"] = chain
+    caching_key = f"{request.route.path.replace('<chain:str>', chain)}"
+    data = await eth_millionaires_dex_caching(request.app, caching_key, caching_ttl, request.args)
+    return Response.success_response(data=data, caching_ttl=caching_ttl, days=-1)
+
+#-----------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
 async def wallet_millionaires_caching(app: object, caching_key: str, caching_ttl:int, request_args: dict) -> any: 
     cache_valid = await cache_validity(app.config.REDIS_CLIENT, caching_key, caching_ttl)
-
     if not cache_valid:
         data = await wallet_millionaires_w_nfts()
         if data:
@@ -38,17 +187,12 @@ async def wallet_millionaires_caching(app: object, caching_key: str, caching_ttl
 
 @WALLET_TAGS_BP.get('<chain>/wallet_millionaires')
 async def wallet_millionaires(request, chain):
-
     caching_ttl =  request.app.config.CACHING_TTL['LEVEL_SEVEN']
-
     if chain not in request.app.config["SUPPORTED_CHAINS"]:
         raise CustomError("chain is required")
     request.args["chain"] = chain
-
     caching_key = f"{request.route.path.replace('<chain:str>', chain)}"
-
     data = await wallet_millionaires_caching(request.app, caching_key, caching_ttl, request.args)
-         
     return Response.success_response(data=data, caching_ttl=caching_ttl, days=-1)
 
 
---
+++ src/wallet_tags/ethereum/token_millionaires_dex.py
@@ -0,0 +1,26 @@
+from shroomdk import ShroomDK
+import os
+
+sdk = ShroomDK(os.getenv("SHROOM_API_KEY"))
+
+async def token_millionaires_dex():
+
+    query = """with wallet_millionaire as (
+      select
+          distinct address
+      from crosschain.core.address_tags
+      where tag_name = 'token millionaire'
+          and creator = 'flipside'
+      )
+      select
+        distinct address,
+        tag_name
+      from crosschain.core.address_tags
+      where
+          tag_type = 'dex'
+          and address in (select distinct address from wallet_millionaire)
+          and creator = 'flipside'
+      """
+
+    query_result_set = sdk.query(query)
+    return query_result_set.records
